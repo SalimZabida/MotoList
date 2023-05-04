6ace810b9c7c5a6e4198f2c0d22633a41a3dba42
SalimZabida <124908896+SalimZabida@users.noreply.github.com>
2023-05-04 20:15:35 +0300

    new app
    starting a new app

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native';
+
+
+const App = () => {
+  return(
+
+  )
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
